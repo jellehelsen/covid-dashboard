a1b588718024eb0ad76745240f5b6edaaf2d3b63
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-23 21:06:21 +0000

    Deploy jellehelsen/covid-dashboard to github.com/jellehelsen/covid-dashboard.git:gh-pages

---
+++ precache-manifest.2053350e2dcfe898d090ded0b8a3ee85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1c1fcb5ae5f0d82696f2f5506fcf91",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "a86fd1c0725ccfea732e",
+    "url": "/covid-dashboard/static/css/main.827ed103.chunk.css"
+  },
+  {
+    "revision": "f85cf325571e39a4a115",
+    "url": "/covid-dashboard/static/js/2.b6cc247e.chunk.js"
+  },
+  {
+    "revision": "ea7b4e38af2835377cf2efe65db8e0ba",
+    "url": "/covid-dashboard/static/js/2.b6cc247e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86fd1c0725ccfea732e",
+    "url": "/covid-dashboard/static/js/main.4bf96f62.chunk.js"
+  },
+  {
+    "revision": "83698af19ff534948b64",
+    "url": "/covid-dashboard/static/js/runtime-main.9226e202.js"
+  }
+]);
